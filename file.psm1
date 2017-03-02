00ae18a8bed91df427808e7e45acc0189af3acfe
Jackbennett <jbennett@birkdalehigh.co.uk>
2017-03-02 12:19:41 +0000

    New function Add-StudentAccess permissions
    Changed student group to a more descriptive allStudents group. Which contains all the year groups.

--- file.psm1
+++ file.psm1
@@ -15,7 +15,7 @@ function Disable-StudentAccess {
         $Path
     )
     Begin {
-        $Deny = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList 'BHS\W7Students', 'FullControl', 'Deny'
+        $Deny = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList 'BHS\AllStudents', 'FullControl', 'Deny'
     }
     Process {
         $Path | Get-Acl | foreach-object {
@@ -35,10 +35,10 @@ function Enable-StudentAccess {
     .SYNOPSIS
         Remove "Deny" permission added by "Enable-StudentAccess"
     .DESCRIPTION
-        Remove the NTFS Deny ACL for a given file for students access.
+        Remove the NTFS Deny ACL for a given item for students access.
     .EXAMPLE
         PS C:\> \\<server\<share>\<Path> | Enable-StudentAccess | Convertto-html | Out-File "Report.html"
-        Generate a report that lists the files students have been blocked from using.
+        Generate a report that lists the item students have been blocked from using.
     #>
     Param(
         [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
@@ -46,14 +46,65 @@ function Enable-StudentAccess {
         $Path
     )
     Begin {
-        $Deny = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList 'BHS\W7Students', 'FullControl', 'Deny'
+        $Rule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList 'BHS\AllStudents', 'FullControl', 'Deny'
     }
     Process {
         $Path | Get-Acl | foreach-object {
-            $psitem.RemoveAccessRule($Deny)
+            $psitem.RemoveAccessRule($Rule) > $null
             try { $psitem | Set-acl }
             catch {
-                Throw "Failed to set permission: $($psitem.path)"
+                Throw "Failed remove Deny permission: $($psitem.path)"
+            }
+            Get-Item $psitem.Path | Write-Output
+        }
+    }
+    End {
+        Write-Warning "Enable-StudentAccess will remove 'Deny' permission set by Disable-StudentAccess. See Add-StudentAccess"
+    }
+}
+
+function Add-StudentAccess {
+    [CmdletBinding()]
+    <#
+    .SYNOPSIS
+        Add "Allow" permission added by "Enable-StudentAccess"
+    .DESCRIPTION
+        Add the NTFS Allow ACL for a given item for students access.
+    .EXAMPLE
+        PS C:\> \\<server\<share>\<Path> | Enable-StudentAccess | Convertto-html | Out-File "Report.html"
+        Generate a report that lists the items students have been blocked from using.
+    #>
+    Param(
+        [Parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [String[]]
+        $Path
+        , # keyword access permission levels
+        [ValidateSet('FullControl','Modify','ReadAndExecute')]
+        [string]
+        $Access = 'ReadAndExecute'
+        , # Sets the Applies to contition for child items
+        [ValidateSet('All','ThisFolder')]
+        [string]
+        $Inherit
+    )
+    Begin {
+        switch ($inherit) {
+            'ThisFolder' { $Inherritance = @('ObjectInherit') }
+            Default { $Inherritance = @('ContainerInherit', 'ObjectInherit') }
+        }
+        $FolderRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList 'BHS\AllStudents', $Access, $Inherritance, 'None', 'Allow'
+        $FileRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList 'BHS\AllStudents', $Access, 'Allow'
+    }
+    Process {
+        $Path | Get-Acl | foreach-object {
+            if ( (Get-Item $Path) -is [System.IO.DirectoryInfo] ){
+                $psitem.SetAccessRule($FolderRule)
+            } else {
+                $psitem.SetAccessRule($FileRule)
+            }
+            try { $psitem | Set-acl }
+            catch {
+                Throw "Failed to add permission: $($psitem.path)"
             }
             Get-Item $psitem.Path | Write-Output
         }
