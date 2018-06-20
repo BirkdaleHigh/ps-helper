434099bb422655dd72c561fe16b24ca4961d8908
Jackbennett <jbennett@birkdalehigh.co.uk>
2018-06-20 11:03:13 +0100

    Filter suggested results on input provided. Clearly indicate the output.

--- file.psm1
+++ file.psm1
@@ -306,8 +306,11 @@ Register-ArgumentCompleter -CommandName 'Add-Access','Remove-Access','Enable-Acc
     }
 
     $preset |
+        Where-Object {
+            $psitem -match $wordToComplete
+        } |
         ForEach-Object {
-            [System.Management.Automation.CompletionResult]::new($psitem, $psitem, 'ParameterValue', ("AD Name: " + $psitem))
+            Write-Output [System.Management.Automation.CompletionResult]::new($psitem, $psitem, 'ParameterValue', ("AD Name: " + $psitem))
         }
 }
 
